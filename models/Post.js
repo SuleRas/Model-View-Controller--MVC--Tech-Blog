92c38b21267e061acb28fa1db8732f55b45dbeac
“SuleRas” <sm.ras@hotmailco.uk>
2022-11-04 17:41:59 +0000

    Created models

---
+++ models/Post.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.STRING,
+    },
+    
+    
+  },
+  {
+    sequelize,
+    
+  }
+);
+
+module.exports = Post;
